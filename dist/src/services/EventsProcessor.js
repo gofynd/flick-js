28bb882582fbf796ca020c167fd4d5bb2b7164dd
nikhilkakade <nikhil.kakade@gofynd.com>
2024-01-28 18:50:05 +0530

    added authorization header

--- dist/src/services/EventsProcessor.js
+++ dist/src/services/EventsProcessor.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.send = void 0;
 var AxiosRequests_1 = require("../utility/AxiosRequests");
 function send(apiKey, payload) {
-    var res = (0, AxiosRequests_1.post)('service/application/webhook/v1.0/click-analytics/events', payload, { 'x-dp-access-token': apiKey });
+    var res = (0, AxiosRequests_1.post)('service/application/webhook/v1.0/click-analytics/events', payload, { 'Authorization': "Bearer ".concat(apiKey) });
     return res;
 }
 exports.send = send;
--- src/services/EventsProcessor.ts
+++ src/services/EventsProcessor.ts
@@ -2,7 +2,7 @@ import { post } from "../utility/AxiosRequests";
 
 
 
- export function  send( apiKey:any, payload:any) {
-        let res = post('service/application/webhook/v1.0/click-analytics/events', payload, { 'x-dp-access-token': apiKey })
-        return res;
-    }
+export function send(apiKey: any, payload: any) {
+    let res = post('service/application/webhook/v1.0/click-analytics/events', payload, { 'Authorization': `Bearer ${apiKey}` })
+    return res;
+}
