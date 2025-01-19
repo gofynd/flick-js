32096d3623375749531eccc97a1987a6dd5d1764
hitendrarathore <hitendrarathore@gofynd.com>
2025-01-19 15:29:08 +0530

    Update version to 1.1.1 to add a check between api calls to handle multiple tabs scenario

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,7 +6,7 @@ export declare var stelioLocal: any;
 export declare var batchExecutorID: any;
 export declare function identify(userID: string, traits: any, emitLoginEvent?: boolean): Promise<void>;
 export declare function reset(): Promise<void>;
-export declare function initialize(endpoint: string, apiKey: any, flushInterval?: number): Promise<void>;
+export declare function initialize(endpoint: string, apiKey: any, flushInterval?: number, apiDurationInterval?: number): Promise<void>;
 export declare function validateClient(apiKey: any): Promise<{
     data: {
         isTokenValid: string;
